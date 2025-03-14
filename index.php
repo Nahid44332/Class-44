3e856ba4e94046be9996dbabc444a90024f9b27b
Nahid44332 <nahidhossen3278@gmail.com>
2025-03-14 00:00:39 -0700

    Class-44

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+
+echo "Welcome To Php"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Class-44</title>
+</head>
+<body>
+    
+    <h1>Hello Hyper pre Prossecor...!</h1>
+
+    <?php
+    echo  "This is our First class on PHP.....";
+
+    echo  "<h3>Do sum with PHP</h3>";
+    $a = 50;
+    $b = 40;
+    
+    $result = $a+$b;
+    
+    echo "<h3>50+40</h3> ";
+    
+    echo "The sum is = ".$result;
+    
+    
+    echo '<h3>Do sub with PHP </h3>';
+    
+    $c = 85;
+    $d = 15;
+    $result = $c-$d;
+    
+    echo "<h3> 85-15 </h3>";
+    echo "The sub is = ".$result;
+    
+    echo "<h3> Do Multiplication with PHP </h3>";
+    
+    $N = 35;
+    $H = 55;
+    
+    $result = $N*$H;
+    
+    echo "<h3> 35*55 </h3>";
+    echo "The mul is = ".$result;
+    echo "<h3> Do Division with PHP </h3>";
+    
+    $m = 25;
+    $n = 5;
+    
+    $result = $m/$n;
+    
+    echo "<h3> 25/5 </h3>";
+    echo "The div is = ".$result."<br><br>";
+    
+    
+    ///Loop///
+    
+    for ($i=0; $i<4; $i++){
+        echo "Hellow PHP! <br>";
+    }
+    
+    
+    
+    ?>
+
+</body>
+</html>
